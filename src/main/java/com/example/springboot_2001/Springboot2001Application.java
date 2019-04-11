fb213549881064ae7e8aee569930be89d20c36b0
Maged Wanis <rania.malak2018fd@gmail.com>
2019-04-11 11:13:37 -0400

    Initial commit

---
+++ src/main/java/com/example/springboot_2001/Springboot2001Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot_2001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2001Application.class, args);
+    }
+
+}
